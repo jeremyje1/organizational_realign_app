22304c35c2f818b63dc19271a3ad6ffb5867f04f
jeremyje1 <jeremy.estrella@gmail.com>
2025-06-19 08:38:19 -0500

    fix: final cleanup of next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-/Users/jeremyestrella/Downloads/organizational_realign_app_specs/organizational_realign_app/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   appDir: 'apps',
