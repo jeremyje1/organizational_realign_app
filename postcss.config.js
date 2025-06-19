04ed68ea2041ed9e1ed4ab5849379ba5a820d846
jeremyje1 <jeremy.estrella@gmail.com>
2025-06-19 09:11:03 -0500

    fix: correct PostCSS plugin shape for Next.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-/** @type {import('postcss-load-config').Config} */
+/** @type {import('postcss').ProcessOptions} */
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
