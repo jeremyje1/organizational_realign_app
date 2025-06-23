5abe6ba42154683edecf15628f1eef1cca5fd37e
jeremyje1 <jeremy.estrella@gmail.com>
2025-06-23 17:00:18 -0500

    build(css): add postcss-import & fix layout imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
-/** @type {import('postcss').Config} */
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    tailwindcss: {},
-    'postcss-nesting': {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+import postcssImport from 'postcss-import';
+import tailwindcss   from 'tailwindcss';
+import nesting       from 'postcss-nesting';
+import autoprefixer  from 'autoprefixer';
+
+export default {
+  plugins: [
+    postcssImport,   // 👈 must be first
+    tailwindcss,
+    nesting,         // Tailwind 4 requires PostCSS 8 nesting plugin
+    autoprefixer
+  ],
 };
