1596a14ead3e04c23a923cfbcca7eeff2db0c886
jeremyje1 <jeremy.estrella@gmail.com>
2025-06-23 16:45:09 -0500

    chore(frontend): lock Tailwind 4.1.10 + PostCSS 8

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 /** @type {import('postcss').Config} */
 module.exports = {
-  plugins: [
-    require("@tailwindcss/postcss"),
-    require("postcss-nesting"),
-    require("autoprefixer"),
-  ],
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    'postcss-nesting': {},
+    autoprefixer: {},
+  },
 };
