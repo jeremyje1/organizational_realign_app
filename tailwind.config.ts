039db0f1a4e0bb2b097998d442057aea5c0b55c4
jeremyje1 <jeremy.estrella@gmail.com>
2025-06-25 16:20:21 -0500

    fix(tailwind): allow bg-gray-50

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
+import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
     './app/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
+    './lib/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        gray: colors.zinc,     // bg-gray-50 -> zinc-50
-        neutral: colors.zinc,  // bg-neutral-50 -> zinc-50
+        gray: colors.neutral,
       },
     },
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
